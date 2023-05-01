7d0d2e152550732c82c0e8b0a0bf9d91d08b61e0
SoujanyaBC <130556946+SoujanyaBC@users.noreply.github.com>
2023-05-01 12:42:31 +0530

    Update HomePage.java

--- main/java/com/webmath/pages/HomePage.java
+++ main/java/com/webmath/pages/HomePage.java
@@ -1,30 +1,26 @@
 package com.webmath.pages;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-
-
-
 import com.webmath.base.TestBase;
 
 public class HomePage extends TestBase {
 
     WebDriver driver;
 	
-	public HomePage(WebDriver driver){
+     public HomePage(WebDriver driver){
       this.driver= driver;
 	}
 	
 	public String validateHomePageTitle() {
-	   return driver.getTitle();
+	return driver.getTitle();
 	}
 	
 	public boolean validateWebmathLogo() {
-	    return driver.findElement(By.xpath(prop.getProperty("webmath_logo"))).isDisplayed();
+	return driver.findElement(By.xpath(prop.getProperty("webmath_logo"))).isDisplayed();
 	}
 	 
 	public void mathForEveryOneLink() {
-		driver.findElement(By.linkText(prop.getProperty("math_for_everyone_locator1"))).click();
+	driver.findElement(By.linkText(prop.getProperty("math_for_everyone_locator1"))).click();
 	
 	
 	}
