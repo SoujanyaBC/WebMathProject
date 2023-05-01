80a5f41e12715636a0a8422b2a98224cb0aca061
SoujanyaBC <130556946+SoujanyaBC@users.noreply.github.com>
2023-05-01 12:33:37 +0530

    Update GeneralMathPageTest.java

--- test/java/com/webmath/testcases/GeneralMathPageTest.java
+++ test/java/com/webmath/testcases/GeneralMathPageTest.java
@@ -1,12 +1,10 @@
 package com.webmath.testcases;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-
 import com.webmath.base.TestBase;
 import com.webmath.pages.GeneralMathPage;
 
@@ -20,20 +18,20 @@ GeneralMathPage generalMathPage;
 	}
 
 	
-    @BeforeMethod
-	public void setup() {
-    initialize();	
-    generalMathPage = new GeneralMathPage(driver);
+         @BeforeMethod
+	 public void setup() {
+         initialize();	
+         generalMathPage = new GeneralMathPage(driver);
     
-    }
+         }
 	
 	
 	 @Test(priority=1)
 	 public void validateFractionToDecimalConversion() {
 	 generalMathPage.fractionToDecimalConversion();
-     WebElement actualResult = driver.findElement(By.xpath(prop.getProperty("actual_output_locator1_gm")));
+         WebElement actualResult = driver.findElement(By.xpath(prop.getProperty("actual_output_locator1_gm")));
 	 boolean actualOutput = actualResult.isDisplayed();
-     Assert.assertTrue(actualOutput);
+         Assert.assertTrue(actualOutput);
 	 
 	}
 	
