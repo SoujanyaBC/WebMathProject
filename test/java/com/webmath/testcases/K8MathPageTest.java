b6a0c6d4e4a76858c90330d34666c6e8855c2647
SoujanyaBC <130556946+SoujanyaBC@users.noreply.github.com>
2023-05-01 12:35:53 +0530

    Update K8MathPageTest.java

--- test/java/com/webmath/testcases/K8MathPageTest.java
+++ test/java/com/webmath/testcases/K8MathPageTest.java
@@ -1,12 +1,10 @@
 package com.webmath.testcases;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-
 import com.webmath.base.TestBase;
 import com.webmath.pages.K8MathPage;
 
@@ -26,10 +24,10 @@ public class K8MathPageTest extends TestBase{
 	 
 	}
 	
-    @Test(priority=1)
+        @Test(priority=1)
 	public void validateMultiplication() {
 	k8MathPage.multiplication();
-    WebElement actualResult = driver.findElement(By.xpath(prop.getProperty("actual_output_locator_1_k8")));
+        WebElement actualResult = driver.findElement(By.xpath(prop.getProperty("actual_output_locator_1_k8")));
 	String actualOutput =  actualResult.getText();
 	String expectedOutput = prop.getProperty("expected_output_1_k8");
 	Assert.assertEquals(actualOutput,expectedOutput);	
@@ -47,32 +45,6 @@ public class K8MathPageTest extends TestBase{
 	public void tearDown() {
 	driver.quit();
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
 	
 
 }
